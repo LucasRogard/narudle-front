f713c5f75668287ad13b59bdcafee9b0def87d92
yughi <yughi.bis@gmail.com>
2024-05-22 11:27:21 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
   return (
       <Router>
           <div className="App">
+              <img src="Narudle_title.png" className="App-logo" alt="logo"/>
               <Routes>
-                  <Route path="/historique" element={<Historique />} />
-                  <Route path="/" element={<Home />} />
+                  <Route path="/historique" element={<Historique/>}/>
+                  <Route path="/" element={<Home/>}/>
               </Routes>
           </div>
       </Router>
--- src/Historique.js
+++ src/Historique.js
@@ -3,7 +3,6 @@ import './App.css';
 function Historique() {
     return (
         <div>
-            <h2>Historique</h2>
         </div>
     );
 }
